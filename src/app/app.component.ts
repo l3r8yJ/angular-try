e79353d8d0898221069ae753f3b523f0e3b165a5
Ivan <clicker.heroes.acg@gmail.com>
2023-01-14 21:44:30 +0300

    strm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {IProduct} from "./models/product";
+import {ProductsService} from "./services/products.service";
+import {Observable, tap} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-try';
+export class AppComponent implements OnInit {
+  title = 'SickFuckTitle';
+  products$: Observable<IProduct[]>
+  loading = false
+
+  constructor(private productsService: ProductsService) {
+  }
+
+  ngOnInit(): void {
+    this.loading = true
+    this.products$ = this.productsService
+    .fetchAll()
+    .pipe(tap(() => this.loading = false))
+  }
 }
