57f5a8e65e73e9fa970405aed4a88d5b7c917a5d
Ivan <clicker.heroes.acg@gmail.com>
2023-01-14 22:16:16 +0300

    filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent implements OnInit {
   title = 'SickFuckTitle';
   products$: Observable<IProduct[]>
   loading = false
+  term = ''
 
   constructor(private productsService: ProductsService) {
   }
